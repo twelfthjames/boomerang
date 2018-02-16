8ae996e6197718f6323c56cfe0f97702c84376eb
Joel Shaw <jshaw@rvadesignsolutions.com>
2018-02-15 21:38:24 -0500

    Test run webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
+const webpack = require('webpack');
+const path = require('path');
+
 module.exports = {
     entry: './src/main.js',
     output: {
-        path: './dist',
+        path: path.resolve(__dirname, 'dist'),
         filename: 'build.js'
     },
     module: {
         loaders: [
             {
                 test: /\.js$/,
-                loader: 'babel',
-                exclude: 'node_modules'
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
             }
         ]
     }
